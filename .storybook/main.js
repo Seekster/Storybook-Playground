2635331eda3c43ebb95a00edcf1c65ab3366f52e
MrDiNz <poo.hu@hotmail.com>
2021-10-04 13:19:54 +0700

    storybook ts

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, './tsconfig.json'),
+        },
+        forkTsCheckerWebpackPluginOptions: {
+          colors: false, // disables built-in colors in logger messages
+        },
+        include: [path.resolve(__dirname, '../src')],
+        transpileManager: true,
+      },
+    },
+  ],
+};
